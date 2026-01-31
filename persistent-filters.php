0b5c25e1c099c004b6ee018b9489cc350d9e72fc
Oscar van Eijk <oscar.van.eijk@oveas.com>
2026-01-31 15:59:55 +0100

    Changed the localization to the officially supported way

--- persistent-filters.php
+++ persistent-filters.php
@@ -7,6 +7,9 @@
  * Description: This plugin enables flexible filter persistence per post type, including a reset button and safeguards. Works for WordPress posts and pages, and Woocommerce products and orders.
  * Author: Oveas Functionality Provider
  * Version: 1.2.0
+ * Text Domain: persistent-filters
+ * Domain Path: /languages
+ * Requires at least: 4.6
  * Tested up to: 6.9
  * License: GPLv2 or later
  * Author URI: http://oveas.com/
@@ -28,18 +31,27 @@ function Persistent_Filters_Uninstall()
 }
 
 /**
+ * Load the textdomain for translations
+ */
+function Persistent_Filters_Load_Textdomain()
+{
+    load_plugin_textdomain('persistent-filters', false, dirname(plugin_basename( __FILE__ )) . '/language');
+}
+
+/**
  * Instantiate this plugin
  */
 function Persistent_Filters_Execute()
 {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/persistent_filters_config.php';
 	require_once plugin_dir_path( __FILE__ ) . 'includes/persistent_filters.php';
-	$locale = apply_filters('persistent_filters_locale', get_locale(), '');
-	load_textdomain('persistent-filters', plugin_dir_path( __FILE__ )  . 'language/' . $locale . '.mo' );
 	$plugin = new Persistent_Filters();
 }
 
 if (is_admin()) {
 	register_uninstall_hook( __FILE__, 'Persistent_Filters_Uninstall' );
+	add_action( 'init', 'Persistent_Filters_Load_Textdomain' );
 	Persistent_Filters_Execute();
 }
+
+
